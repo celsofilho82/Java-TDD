a9862d4c2fa2a5aaac8fb85db6408e7d4445b233
Celso Ribeiro <celsofilho82@gmail.com>
2021-02-23 16:51:19 -0300

    Introdução aos testes unitários

--- TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -7,9 +7,10 @@ import java.util.Date;
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoService {
-	
+
 	public Locacao alugarFilme(Usuario usuario, Filme filme) {
 		Locacao locacao = new Locacao();
 		locacao.setFilme(filme);
@@ -17,18 +18,43 @@ public class LocacaoService {
 		locacao.setDataLocacao(new Date());
 		locacao.setValor(filme.getPrecoLocacao());
 
-		//Entrega no dia seguinte
+		// Entrega no dia seguinte
 		Date dataEntrega = new Date();
 		dataEntrega = adicionarDias(dataEntrega, 1);
 		locacao.setDataRetorno(dataEntrega);
-		
-		//Salvando a locacao...	
-		//TODO adicionar método para salvar
-		
+
+		// Salvando a locacao...
+		// TODO adicionar método para salvar
+
 		return locacao;
 	}
 
 	public static void main(String[] args) {
-		
+
+		/*
+		 * Testes devem ser FIRST 
+		 * Fast (Rápido) 
+		 * Independent (Independente)
+		 * Repeatables (Pode ser executado quantas vezes for necessário)
+		 * Self-Verifying (Auto verificavel)
+		 * Timely (Oportuno deve ser criado no momento correto)
+		 */
+
+		// Criando o cenário
+
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Celso Ribeiro");
+		Filme filme = new Filme("COMA", 10, 2.50);
+
+		// Realizadno a ação
+
+		Locacao locacao = service.alugarFilme(usuario, filme);
+
+		// Fazendo a verificação
+
+		System.out.println(locacao.getValor() == 2.5);
+		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+
 	}
 }
