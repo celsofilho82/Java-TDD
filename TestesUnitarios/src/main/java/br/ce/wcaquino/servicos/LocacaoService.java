ea7bd8b46c7ddcb3ea9c2a1ed7a71ec9a251bbde
Celso Ribeiro <celsofilho82@gmail.com>
2021-02-24 14:55:05 -0300

    Desafio: Trabalhando com uma lista de filmes

--- TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -2,7 +2,9 @@ package br.ce.wcaquino.servicos;
 
 import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -12,20 +14,25 @@ import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoService {
 
-	public Locacao alugarFilme(Usuario usuario, Filme filme) throws Exception {
-		if (filme.getEstoque() == 0) {
-			throw new Exception("Filme sem estoque!");
+	public Locacao alugarFilme(Usuario usuario, List<Filme> filmes) throws Exception {
+		for (Filme filme : filmes) {
+			if (filme.getEstoque() == 0) {
+				throw new Exception("Filme sem estoque!");
+			}
 		}
-		
+
 		if (usuario == null) {
 			throw new LocadoraException("Usuaŕio não pode ser nulo");
 		}
-		
+
 		Locacao locacao = new Locacao();
-		locacao.setFilme(filme);
+		locacao.setFilme(filmes);
 		locacao.setUsuario(usuario);
 		locacao.setDataLocacao(new Date());
-		locacao.setValor(filme.getPrecoLocacao());
+		Double valorFilme = 0d;
+		for (Filme filme : filmes) {
+			valorFilme += filme.getPrecoLocacao();
+		}
 
 		// Entrega no dia seguinte
 		Date dataEntrega = new Date();
@@ -41,12 +48,9 @@ public class LocacaoService {
 	public static void main(String[] args) throws Exception {
 
 		/*
-		 * Testes devem ser FIRST 
-		 * Fast (Rápido) 
-		 * Independent (Independente) 
-		 * Repeatables (Pode ser executado quantas vezes for necessário)
-		 * Self-Verifying (Auto verificavel) 
-		 * Timely (Oportuno deve ser criado no momento correto)
+		 * Testes devem ser FIRST Fast (Rápido) Independent (Independente) Repeatables
+		 * (Pode ser executado quantas vezes for necessário) Self-Verifying (Auto
+		 * verificavel) Timely (Oportuno deve ser criado no momento correto)
 		 */
 
 		// Criando o cenário
@@ -54,10 +58,11 @@ public class LocacaoService {
 		LocacaoService service = new LocacaoService();
 		Usuario usuario = new Usuario("Celso Ribeiro");
 		Filme filme = new Filme("COMA", 10, 2.50);
-
+		List<Filme> filmes = new ArrayList<Filme>();
+		filmes.add(filme);
 		// Realizadno a ação
 
-		Locacao locacao = service.alugarFilme(usuario, filme);
+		Locacao locacao = service.alugarFilme(usuario, filmes);
 
 		// Fazendo a verificação
 
